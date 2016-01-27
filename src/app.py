f759ca5986a711f8ee0f14a8435def8e5736637e
Vishal Talwar <vishal@grainlighting.com>
2016-01-26 22:24:14 -0800

    Fixing relative path issues

--- src/app.py
+++ src/app.py
@@ -1,20 +1,37 @@
-from flask import Flask, request, send_from_directory
+import os
+import logging
+
+from flask import Flask
+from flask import redirect, request, send_from_directory, url_for
+
 
 app = Flask(__name__)
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+SVG_DIR = os.path.join(ROOT_DIR, '..', 'data', 'kanji')
+
 
 def kanji_to_filename(kanji):
     return "{:05x}.svg".format(ord(kanji[0]))
 
-@app.route('/svg/<codepoint>')
-def raw_svg(codepoint):
-    return send_from_directory('../data/kanji',
-                               codepoint)
+
+@app.route('/')
+def index():
+    return redirect(url_for('lookup', kanji='我'))
+
+
+@app.route('/svg/<filename>')
+def raw_svg(filename):
+    logging.info("Reading file {}".format(filename))
+    return send_from_directory(SVG_DIR, filename)
+
 
 @app.route('/lookup/<kanji>')
 def lookup(kanji):
-    return send_from_directory('../data/kanji',
-                               kanji_to_filename(kanji))
+    filename = kanji_to_filename(kanji)
+    logging.info("Reading file {}".format(os.path.join(SVG_DIR, filename)))
+    return send_from_directory(SVG_DIR, filename)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     app.run(debug=True)
