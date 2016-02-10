5a92ffad7680bcc1a7a1257d3d1bb009b2bb7b8a
Vishal Talwar <rawlatv@gmail.com>
2016-02-09 20:05:06 -0800

    Fixing path bugs in raw svg controller

--- app/svg.py
+++ app/svg.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import current_app, Blueprint, redirect, render_template,\
-                  send_from_directory, url_for
+                  send_file, url_for
 
 import app.lib_kanji as libk
 
@@ -17,18 +17,18 @@ def index():
 @svg.route('/static/<filename>')
 def svg_static(filename):
     current_app.logger.info("Loading file {}".format(filename))
-    return send_from_directory(libk.SVG_DIR, filename)
+    return send_file(filename)
 
 
 @svg.route('/raw/<kanji>')
 def svg_kanji_raw(kanji):
-    filename = libk.kanji_to_filename(kanji)
-    current_app.logger.info("Loading file {}".format(os.path.join(SVG_DIR, filename)))
-    return send_from_directory(libk.SVG_DIR, filename)
+    filename = libk.path_to_kanji(kanji)
+    current_app.logger.info("Loading file {}".format(filename))
+    return send_file(filename)
 
 
 @svg.route('/pretty/<kanji>')
 def svg_kanji_pretty(kanji):
-    filename = libk.kanji_to_filename(kanji)
-    current_app.logger.info("Loading file {}".format(os.path.join(SVG_DIR, filename)))
-    return send_from_directory(libk.SVG_DIR, filename)
+    filename = libk.path_to_kanji(kanji)
+    current_app.logger.info("Loading file {}".format(filename))
+    return send_file(filename)
